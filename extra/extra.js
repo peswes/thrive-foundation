283d1035c424b0be57b478fb20f98459a23cc9b5
peswes <ayomikunleadejuwon@gmail.com>
2024-12-20 09:19:17 -0800

    Delete extra directory

--- extra/extra.js
+++
@@ -1,7 +0,0 @@
-
-const shareOption = document.querySelector('.share-option');
-
-document.querySelector('.share-button').addEventListener('click', function () {
-  this.classList.toggle('active');
-  shareOption.classList.toggle('active');
-});
